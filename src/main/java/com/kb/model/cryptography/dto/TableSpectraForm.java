9eca320ab1c1d58f54bd1c2e5946569b74d49992
Katyushka <katya94fm@mail.ru>
2015-10-12 01:12:12 +0500

    add spectra form

---
+++ src/main/java/com/kb/model/cryptography/dto/TableSpectraForm.java
@@ -0,0 +1,22 @@
+package com.kb.model.cryptography.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 06.10.15.
+ */
+public class TableSpectraForm {
+
+    private List<List<String>> table = new ArrayList<>();
+
+    public List<List<String>> getTable() {
+        return table;
+    }
+
+    public void setTable(List<List<String>> table) {
+        this.table = table;
+    }
+
+
+}
